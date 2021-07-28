5399b0d2e1febfbaa53661ffe98e731f15cda59a
TereshchenkoSD <serghh1988@gmail.com>
2021-07-28 16:49:30 +0300

    Добавил в index.html отдельный div для рендера модального окна. Исправил ошибку в коде компонента Button. Переделал логику компонента Modal на хуках.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,48 +2,45 @@ import { createPortal } from 'react-dom';
 
 import PropTypes from 'prop-types';
 
-import { Component } from 'react';
+import React, { useEffect } from 'react';
 
 import { Overlay, ModalContainer } from './Modal.styles';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleBackdropClick = e => {
+export default function Modal({ onClose, largeImageURL, tags }) {
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <ModalContainer>
-          <img src={this.props.largeImageURL} alt={this.props.tags} />
-        </ModalContainer>
-      </Overlay>,
-      modalRoot,
-    );
-  }
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    console.log('Повесил слушателя');
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      console.log('Снял слушателя');
+    };
+  });
+
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <ModalContainer>
+        <img src={largeImageURL} alt={tags} />
+      </ModalContainer>
+    </Overlay>,
+    modalRoot,
+  );
 }
 
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
   largeImageURL: PropTypes.string.isRequired,
 };
-
-export default Modal;
