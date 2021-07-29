002bec2bc59ca9c76999ed40d199cf8d11796eab
TereshchenkoSD <serghh1988@gmail.com>
2021-07-29 11:16:04 +0300

    Переписал код компонента App на хуках.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import toast, { Toaster } from 'react-hot-toast';
 
@@ -15,65 +15,61 @@ import Modal from './components/Modal';
 import { AppContainer } from './App.styles';
 import LoadMoreButton from './components/Button';
 
-export default class App extends Component {
-  state = {
-    imageName: null,
-    images: [],
-    reqStatus: 'idle',
-    page: 1,
-    showModal: false,
-    tags: '',
-    selectedImage: '',
-    error: null,
-  };
+const App = () => {
+  const [imageName, setImageName] = useState(null);
+  const [images, setImages] = useState([]);
+  const [reqStatus, setReqStatus] = useState('idle');
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+  const [error, setError] = useState(null);
 
-  componentDidUpdate(_, prevState) {
-    const { imageName, page } = this.state;
-    const shouldFetch =
-      prevState.imageName !== imageName || prevState.page !== page;
-    if (shouldFetch) {
-      this.setState({ reqStatus: 'pending' });
-      this.onFetchImages();
+  const showImages = images.length > 11;
+
+  useEffect(() => {
+    if (!imageName) {
+      return;
     }
+    setReqStatus('pending');
+
+    async function onFetchImages() {
+      try {
+        const images = await fetchImages(page, imageName);
+        setImages(prevState => [...prevState, ...images]);
+        setReqStatus('resolved');
+      } catch (error) {
+        setReqStatus('rejected');
+        setError(true);
+        toast.error('Ooops, there is no such image');
+      }
+    }
+
+    onFetchImages();
 
     if (page > 1) {
-      this.scrollPageToEnd();
+      scrollPageToEnd();
     }
-  }
+  }, [page, imageName]);
 
-  async onFetchImages() {
-    const { imageName, page } = this.state;
-    try {
-      const images = await fetchImages(page, imageName);
-      this.setState(prevState => ({
-        images: [...prevState.images, ...images],
-        reqStatus: 'resolved',
-      }));
-    } catch (error) {
-      this.setState({ reqStatus: 'rejected', error: true });
-      toast.error('Ooops, there is no such image');
-    }
-  }
-
-  handleFormSubmit = imageName => {
+  const handleFormSubmit = imageName => {
     if (imageName.trim() === '') {
       toast.error('Invalid search query');
       return;
     }
-    this.resetState();
-    this.setState({ imageName });
+    resetState();
+    setImageName(imageName);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  handleSelectedImage = data => {
-    this.setState({ selectedImage: data });
-    this.toggleModal();
+  const handleSelectedImage = data => {
+    setSelectedImage(data);
+    toggleModal();
   };
 
-  scrollPageToEnd = () => {
+  const scrollPageToEnd = () => {
     setTimeout(() => {
       window.scrollTo({
         top: document.documentElement.scrollHeight,
@@ -82,37 +78,30 @@ export default class App extends Component {
     }, 1000);
   };
 
-  onLoadMoreBtn = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const onLoadMoreBtn = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  resetState = () => {
-    this.setState({
-      imageName: null,
-      page: 1,
-      images: [],
-      status: 'idle',
-    });
+  const resetState = () => {
+    setImageName(null);
+    setPage(1);
+    setImages([]);
+    setReqStatus('idle');
   };
 
-  render() {
-    const { images, showModal, selectedImage, reqStatus, error } = this.state;
-    const showImages = images.length > 11;
+  return (
+    <AppContainer>
+      {error && toast.error('No such pictures on the server!')}
+      <Searchbar onSearch={handleFormSubmit} />
+      {reqStatus === 'pending' && <Loader />}
+      <ImageGallery onSelect={handleSelectedImage} images={images} />
+      {showImages && <LoadMoreButton onClick={onLoadMoreBtn} />}
+      {showModal && (
+        <Modal onClose={toggleModal} largeImageURL={selectedImage} />
+      )}
+      <Toaster position="top-right" />
+    </AppContainer>
+  );
+};
 
-    return (
-      <AppContainer>
-        {error && toast.error('No such pictures on the server!')}
-        <Searchbar onSearch={this.handleFormSubmit} />
-        {reqStatus === 'pending' && <Loader />}
-        <ImageGallery onSelect={this.handleSelectedImage} images={images} />
-        {showImages && <LoadMoreButton onClick={this.onLoadMoreBtn} />}
-        {showModal && (
-          <Modal onClose={this.toggleModal} largeImageURL={selectedImage} />
-        )}
-        <Toaster position="top-right" />
-      </AppContainer>
-    );
-  }
-}
+export default App;
